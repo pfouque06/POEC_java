3eb7b02d064a9b3699e7ad7102087f43f10919ad
pfouque06 <pfouque@gmail.com>
2020-01-03 16:13:47 +0100

    improve exo022_optimized

--- src/exo018/Exo022_EZCalculette_optimized.java
+++ src/exo018/Exo022_EZCalculette_optimized.java
@@ -7,78 +7,88 @@ import scanTools.ScanTools;
 // mini calculette
 public class Exo022_EZCalculette_optimized {
 
+	static void displayMenu() {
+
+		System.out.println();
+		System.out.println("\t\tMENU PRINCIPAL");
+		System.out.println();
+		System.out.println("\t1. Additionner deux nombres");
+		System.out.println("\t2. Soustraire deux nombres");
+		System.out.println("\t3. Multiplier deux nombres");
+		System.out.println("\t4. Diviser deux nombres");
+		System.out.println("\t5. \"Moduler\" deux nombres ;-)");
+		System.out.println("\t6. Calculer la moyenne de deux nombres, so what ??");
+		System.out.println("\t0. quitter");
+		System.out.println();
+	}
+
+	static void processAction(String pAction) {
+
+		int[] values = new int[2];
+		System.out.println();
+		values[0] = ScanTools.scanInt("Entrez le nombre a : ");
+		// System.out.println("nb 1="+nb[0]);
+		values[1] = ScanTools.scanInt("Entrez le nombre b : ");
+		// System.out.println("nb 2="+nb[1]);
+
+
+		System.out.println();
+		switch (pAction) {
+		case "1":
+			int sum = values[0] + values[1];
+			System.out.println("\t" + values[0] + " + " + values[1] + " = " + sum);
+			break;
+		case "2":
+			int diff = values[0] - values[1];
+			System.out.println("\t" + values[0] + " - " + values[1] + " = " + diff);
+			break;
+		case "3":
+			int mult = values[0] * values[1];
+			System.out.println("\t" + values[0] + " x " + values[1] + " = " + mult);
+			break;
+		case "4":
+			if (values[1] == 0) {
+				System.out.println("\tERROR : le nombre b est nul, division par 0 est impossible");
+				break;
+			}
+			float div = (float) values[0] / values[1];
+			System.out.println("\t" + values[0] + " / " + values[1] + " = " + String.format("%.2f", div));
+			break;
+		case "5":
+			if (values[1] == 0) {
+				System.out.println("\tERROR : le nombre b est nul, division par 0 est impossible");
+				break;
+			}
+			int modulo = values[0] % values[1];
+			System.out.println("\t" + values[0] + " % " + values[1] + " = " + modulo);
+			break;
+		case "6":
+			float average = (float) (values[0] + values[1]) / 2;
+			System.out.println("\t( " + values[0] + " + " + values[1] + " ) / 2 = " + String.format("%.2f", average));
+			break;
+		default:
+			System.out.println("choix invalide");
+		}
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		// Scanner scan = new Scanner(System.in);
 		String buffer = "";
 		int[] nb = { 0, 0 };
 		do {
-			System.out.println();
-			System.out.println("\t\tMENU PRINCIPAL");
-			System.out.println();
-			System.out.println("\t1. Additionner deux nombres");
-			System.out.println("\t2. Soustraire deux nombres");
-			System.out.println("\t3. Multiplier deux nombres");
-			System.out.println("\t4. Diviser deux nombres");
-			System.out.println("\t5. \"Moduler\" deux nombres ;-)");
-			System.out.println("\t6. Calculer la moyenne de deux nombres, so what ??");
-			System.out.println("\t0. quitter");
-			System.out.println();
+			displayMenu();
 			buffer = ScanTools.scanMatchedBuffer("\tFaites votre choix : ", "[0-6]");
 			// System.out.println("buffer="+buffer);
 
 			if (!buffer.equals("0")) {
 
-				System.out.println();
-				nb[0] = ScanTools.scanInt("Entrez le nombre a : ");
-				// System.out.println("nb 1="+nb[0]);
-				nb[1] = ScanTools.scanInt("Entrez le nombre b : ");
-				// System.out.println("nb 2="+nb[1]);
+				processAction(buffer);
 
-				System.out.println();
-				switch (buffer) {
-				case "1":
-					int sum = nb[0] + nb[1];
-					System.out.println("\t" + nb[0] + " + " + nb[1] + " = " + sum);
-					break;
-				case "2":
-					int diff = nb[0] - nb[1];
-					System.out.println("\t" + nb[0] + " - " + nb[1] + " = " + diff);
-					break;
-				case "3":
-					int mult = nb[0] * nb[1];
-					System.out.println("\t" + nb[0] + " x " + nb[1] + " = " + mult);
-					break;
-				case "4":
-					if (nb[1] == 0) {
-						System.out.println("\tERROR : le nombre b est nul, division par 0 est impossible");
-						break;
-					}
-					float div = (float) nb[0] / nb[1];
-					System.out.println("\t" + nb[0] + " / " + nb[1] + " = " + String.format("%.2f", div));
-					break;
-				case "5":
-					if (nb[1] == 0) {
-						System.out.println("\tERROR : le nombre b est nul, division par 0 est impossible");
-						break;
-					}
-					int modulo = nb[0] % nb[1];
-					System.out.println("\t" + nb[0] + " % " + nb[1] + " = " + modulo);
-					break;
-				case "6":
-					float average = (float) (nb[0] + nb[1]) / 2;
-					System.out.println("\t( " + nb[0] + " + " + nb[1] + " ) / 2 = " + String.format("%.2f", average));
-					break;
-				default:
-					System.out.println("choix invalide");
-				}
 			}
 		} while (!buffer.equals("0"));
 
 		System.out.println("\texiting...");
-		// scan.close();
 	}
 
 }
