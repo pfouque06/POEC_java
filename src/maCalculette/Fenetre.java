f1cfdc00a683fe1b356c5b007e43261f99f471b3
pfouque06 <pfouque@gmail.com>
2020-01-20 17:08:32 +0100

    maCalculette: keybindings Work in Progress

--- src/maCalculette/Fenetre.java
+++ src/maCalculette/Fenetre.java
@@ -6,12 +6,17 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.io.File;
 import java.io.PrintStream;
+
+import javax.swing.AbstractAction;
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.KeyStroke;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
 
@@ -27,12 +32,14 @@ public class Fenetre extends JFrame implements ActionListener {
 	private JPanel labelPan = new JPanel();
 	private JLabel label = new JLabel("0");
 	private JPanel keyboardPan = new JPanel();
+	private String[] kNames = { "7", "8", "9", "4", "5", "6", "1", "2", "3", "0", ".", "<"} ;
+	private int[] kKeys = { KeyEvent.VK_7, KeyEvent.VK_8, KeyEvent.VK_9, KeyEvent.VK_4, KeyEvent.VK_5, KeyEvent.VK_6, KeyEvent.VK_1, KeyEvent.VK_2, KeyEvent.VK_3, KeyEvent.VK_0, KeyEvent.VK_PERIOD, KeyEvent.VK_BACK_SPACE} ;
 	//private String[] kNames = { "7", "8", "9", "4", "5", "6", "1", "2", "3", "0", ".", "C"};
-	private String[] kNames = { "7", "8", "9", "4", "5", "6", "1", "2", "3", "0", ".", "<"};
 	private JButton[] kButtons = new JButton[12];
 	private JPanel ActionPan = new JPanel();
 	//private String[] aNames = { "/", "%", "x", "MS", "-", "MC", "+", "="};
 	private String[] aNames = { "/", "C", "x", "MC", "-", "MS", "+", "="};
+	private int[] aKeys = { KeyEvent.VK_DIVIDE, -1, KeyEvent.VK_MULTIPLY, -1, KeyEvent.VK_SUBTRACT, -1, KeyEvent.VK_ADD, KeyEvent.VK_EQUALS};
 	private JButton[] aButtons = new JButton[8];
 	int indexMS = 0;
 
@@ -41,6 +48,49 @@ public class Fenetre extends JFrame implements ActionListener {
 	public Fenetre() {
 		// TODO Auto-generated constructor stub
 
+		AbstractAction keybindAction = new AbstractAction() {
+			@Override
+			public void actionPerformed(ActionEvent ae) {
+				//String buttonTitle = "";
+				String buttonTitle =  ae.getActionCommand();
+				System.out.println(">> keybindAction.actionPerformed(" + buttonTitle + ")");
+				int index;
+				switch (buttonTitle) {
+				case ".":
+				case "0":
+				case "1":
+				case "2":
+				case "3":
+				case "4":
+				case "5":
+				case "6":
+				case "7":
+				case "8":
+				case "9":
+				case "<": // find backspace keyboard key
+					index = 0;
+					while (index < kNames.length && (!kNames[index].equals(buttonTitle)))
+						index++;
+					kButtons[index].doClick();
+					break;
+				case "+":
+				case "-":
+				case "x":
+				case "/":
+				case "%":
+				case "=":
+					index = 0;
+					while (index < aNames.length && (!aNames[index].equals(buttonTitle)))
+						index++;
+					aButtons[index].doClick();
+					break;
+				default:
+					break;
+				}
+				//core.processAction(buttonTitle);
+			}
+		};
+		
 		// On initialise la JFrame
 		this.setTitle("Calculette");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -63,6 +113,9 @@ public class Fenetre extends JFrame implements ActionListener {
 			kButtons[indexCell].setFont(kFont);
 			kButtons[indexCell].addActionListener(this); // l'observer est notre fenetre
 			keyboardPan.add(kButtons[indexCell]);
+			// key assocation
+			kButtons[indexCell].getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(kKeys[indexCell], 0), kNames[indexCell]);
+			kButtons[indexCell].getActionMap().put(kNames[indexCell], keybindAction);
 		}
 		
 		ActionPan.setPreferredSize(new Dimension(125, 225));
@@ -76,41 +129,10 @@ public class Fenetre extends JFrame implements ActionListener {
 			//aButtons[indexCell].setFont(kFont);
 			aButtons[indexCell].addActionListener(this); // l'observer est notre fenetre
 			ActionPan.add(aButtons[indexCell]);
+			// key assocation
+			aButtons[indexCell].getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(aKeys[indexCell], 0), aNames[indexCell]);
+			aButtons[indexCell].getActionMap().put(aNames[indexCell], keybindAction);
 		}
-		
-		////Dimension dim = new Dimension(55, 40);
-		//Box[] kHBox = new Box[4];
-		//Box kBox = Box.createVerticalBox();
-		//for (int indexCell = 0, indexBox = -1; indexCell < kNames.length ; indexCell++) {
-		//	kButtons[indexCell] = new JButton(kNames[indexCell]);
-		//	//kButtons[indexCell].setPreferredSize(dim);
-		//	kButtons[indexCell].addActionListener(this); // l'observer est notre fenetre
-		//	if ( (indexCell % 3) == 0)
-		//		kHBox[++indexBox] = Box.createHorizontalBox();
-		//	kHBox[indexBox].add(kButtons[indexCell]);
-		//}
-		//kBox.add(kHBox[0]);
-		//kBox.add(kHBox[1]);
-		//kBox.add(kHBox[2]);
-		//kBox.add(kHBox[3]);
-		//keyboardPan.add(kBox);
-
-		//Dimension dim2 = new Dimension(50, 40);
-		//Box[] aHBox = new Box[4];
-		//Box aBox = Box.createVerticalBox();
-		//for (int indexCell = 0, indexBox = -1; indexCell < aNames.length ; indexCell++) {
-		//	aButtons[indexCell] = new JButton(aNames[indexCell]);
-		//	//aButtons[indexCell].setPreferredSize(dim2);
-		//	aButtons[indexCell].addActionListener(this); // l'observer est notre fenetre
-		//	if ( (indexCell % 2) == 0)
-		//		aHBox[++indexBox] = Box.createHorizontalBox();
-		//	aHBox[indexBox].add(aButtons[indexCell]);
-		//}
-		//aBox.add(aHBox[0]);
-		//aBox.add(aHBox[1]);
-		//aBox.add(aHBox[2]);
-		//aBox.add(aHBox[3]);
-		//ActionPan.add(aBox);
 
 		//debug syso
 		//logger.println("kButtons[0].getSize() = " + kButtons[0].getSize());
